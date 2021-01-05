1ba2b3c92c1b50c7e731d9198ee6dd9c7d2c056e
alvinau0427 <alvinau0427@outlook.com>
2021-01-06 07:17:06 +0800

    Initial commit

---
+++ app/app.module.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Define the `phonecatApp` module
+angular.module('phonecatApp', [
+	// ...which depends on the `phoneList` module
+	'ngAnimate',
+	'ngRoute',
+	'core',
+	'phoneDetail',
+	'phoneList'
+]);
